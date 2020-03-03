353f08b9dd026ea0edffe6f9de005473e40f48b4
slawry00 <lawry@calpoly.edu>
2020-03-03 01:45:26 -0800

    finished project2_funcs

--- Project2/Project2_funcs.py
+++ Project2/Project2_funcs.py
@@ -1,8 +1,19 @@
 import numpy as np
+import networkx as nx
 import math
 
 # An edgelist is a dictionary of sets. The dictionary key is the vertex and dictionary value is
 # a set that contains all of its adjacent vertices.
+
+
+# edgelist -> networkx graph
+# converts an edgelist into an networkx graph
+def convert_edgelist_to_nx(edgelist):
+    G = nx.Graph()
+    for vert in edgelist:
+        for neighbor in edgelist[vert]:
+            G.add_edge(vert, neighbor)
+    return G
 
 
 # 2 column csv_file -> edgelist
@@ -25,7 +36,68 @@ def make_edgelist(my_file):
                 my_dict[data_l[1]] = {data_l[0]}
     return my_dict
 
-# edgelist -> int
+# edgelist + vertex -> int
 # given an edgelist and a vertex, determines the degree of that vertex
 def get_degree(edgelist, vert):
     return len(edgelist[vert])
+
+# edgelist + vertex -> float
+# given an edgelist and a vertex, determines the clustering coefficient of that vertex
+def clust_c_v(edgelist, vert):
+    edge_count = 0
+    num_neighbors = len(edgelist[vert])
+    for i in edgelist[vert]: # set of neighbors
+        for j in edgelist[i]: # neighbor's neighbors
+            if j in edgelist[vert]:
+                edge_count += 1
+    return edge_count/float((num_neighbors)*(num_neighbors-1))
+
+# edgelist + vertex -> float
+# given an edgelist, determines the clustering coefficient of the entire graph
+# the clustering coefficient of a graph is the average clustering coefficient of its verts
+def clust_c_g(edgelist):
+    clust_sum = 0
+    for vert in edgelist:
+        clust_sum += clust_c_v(edgelist, vert)
+    return clust_sum/len(edgelist)
+
+# edgelist + vertex -> float
+# given an edgelist and a vertex, determines the closeness centrality of that vertex
+def closeness_centrality(edgelist, vert):
+    short_sum = 0
+    short_paths = nx.shortest_path_length(convert_edgelist_to_nx(edgelist), vert)
+    for value in short_paths.values():
+        short_sum += value
+    return 1.0/short_sum
+
+
+# edgelist + vertex -> float
+# given an edgelist and a vertex, determines the betweenness centrality of that vertex
+def betweenness_centrality(edgelist, vert):
+    bet_sum = 0.0
+    for i, val1 in enumerate(edgelist):
+        for j, val2 in enumerate(edgelist):
+            if j>i and vert != val1 and vert != val2:
+                all_shorts = nx.all_shortest_paths(edgelist, val1, val2)
+                vert_in_short_count = 0
+                short_count = 0
+                for short in all_shorts:
+                    short_count += 1
+                    if vert in short:
+                        vert_in_short_count += 1
+                bet_sum += (vert_in_short_count)/short_count
+    return bet_sum
+
+
+# edgelist -> float
+# given an edgelist, determines the average shortest path length of the graph
+def avg_short_path(edgelist):
+    num_nodes= len(edgelist)
+    short_sum = 0
+    short_paths = nx.shortest_path_length(convert_edgelist_to_nx(edgelist))
+    for vert in short_paths:
+        for path_l in vert[1].values(): #its a tuple of (vertex, {neighbor : length})
+            short_sum += path_l
+    return short_sum/float((num_nodes)*(num_nodes-1)) #total_sum/tot_nodes
+
+
--- Project2/Project2_tests.py
+++ Project2/Project2_tests.py
@@ -2,11 +2,22 @@ import unittest
 import math
 import numpy as np
 from Project2_funcs import *
+import networkx as nx
 
 small_e = make_edgelist("test_file.csv")
-print(small_e)
 big_e = make_edgelist("proj2_data/musae_git_edges.csv")
 
+small_nx_e = convert_edgelist_to_nx(small_e)
+big_nx_e = convert_edgelist_to_nx(big_e)
+
+#small_nx_data = open('test_file.csv', "r")
+#small_nx_data.readline()
+#small_nx_e = nx.read_edgelist(small_nx_data, delimiter = ',', nodetype=int)
+#
+#big_nx_data = open('proj2_data/musae_git_edges.csv', "r")
+#big_nx_data.readline()
+#big_nx_e = nx.read_edgelist(big_nx_data, delimiter = ',', nodetype=int)
+#
 class TestEdgeList(unittest.TestCase):
     def test_symmetric(self):
         self.assertTrue(1 in small_e[5])
@@ -16,31 +27,47 @@ class TestGetDegree(unittest.TestCase):
     def test_degree_simple(self):
         self.assertEqual(get_degree(small_e, 5), 3)
     def test_degree_more(self):
-        self.assertEqual(get_degree(big_e, 831), 15)
+        self.assertEqual(get_degree(big_e, 831), nx.degree(big_nx_e, 831))
+
+class TestClustCV(unittest.TestCase):
+    def test_clustcv_simple(self):
+        self.assertEqual(clust_c_v(small_e, 5), 2.0/3)
+    def test_clustcv_more(self):
+        self.assertEqual(clust_c_v(big_e, 831), nx.clustering(big_nx_e, 831))
+
+class TestClustCG(unittest.TestCase):
+    def test_clustcg_simple(self):
+        self.assertEqual(clust_c_g(small_e), nx.average_clustering(small_nx_e))
+    def test_clustcv_more(self):
+        self.assertEqual(clust_c_g(small_e), nx.average_clustering(small_nx_e))
+
+class TestCloseCent(unittest.TestCase):
+    def test_closecent_simple(self):
+        self.assertEqual(closeness_centrality(small_e, 5),
+                         nx.closeness_centrality(small_nx_e, 5)/(len(small_nx_e)-1))
+    def test_closecent_more(self):
+        self.assertEqual(closeness_centrality(big_e, 831),
+                         nx.closeness_centrality(big_nx_e, 831)/(len(big_nx_e)-1))
+
+class TestBetCent(unittest.TestCase):
+    def test_bet_cent_simple(self):
+        self.assertEqual(betweenness_centrality(small_e, 5),
+                         nx.betweenness_centrality(small_nx_e, normalized=False)[5])
+    # cant do big graph. Takes too long
+    #def test_bet_cent_more(self):
+    #    self.assertEqual(betweenness_centrality(big_e, 831),
+    #                     nx.betweenness_centrality(big_nx_e, normalized=False)[831])
 
 
 
-# Just to view the template, I copied some in here
-#class TestMyVar(unittest.TestCase):
-#    def test_my_var(self):
-#        arr1 = np.array([5,3,10])
-#        ans = 13
-#        self.assertEqual(my_var(arr1), ans)
-#
-#class TestMeans(unittest.TestCase):
-#    def test_means_basic(self):
-#        my_arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
-#        ans = np.array([2,5,8])
-#        np.testing.assert_array_equal(col_means(my_arr), ans)
-#    def test_means_nans(self):
-#        my_arr = np.array([[None,None,None],[4,5,6],[None,8,None]])
-#        ans = np.array([np.nan,5,8])
-#        np.testing.assert_array_equal(col_means(my_arr), ans)
-#    def test_one_dim(self):
-#        my_arr = np.array([[1,2,3]])
-#        ans = np.array([2])
-#        #print(col_means(my_arr))
-#        np.testing.assert_array_equal(col_means(my_arr), ans)
+class TestAvgShortPath(unittest.TestCase):
+    def test_avg_short_path_simple(self):
+        self.assertEqual(avg_short_path(small_e),
+                         nx.average_shortest_path_length(small_nx_e))
+    # cant do big graph. Takes too long
+    #def test_avg_short_path_more(self):
+    #    self.assertEqual(avg_short_path(big_e),
+    #                     nx.average_shortest_path_length(big_nx_e))
 
 
 if __name__ == '__main__':
